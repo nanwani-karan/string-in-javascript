852bcb41e20c0d16208fda3f2d97518ecfd8d8a2
nanwani-karan <nanwanik21@gmail.com>
2025-05-23 07:19:37 +0000

    maths and num

---
+++ 01_basics/numsmaths.js
@@ -0,0 +1,13 @@
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.3));
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.8));
+//console.log(Math.min(4,5,6,3,2,1));
+//console.log(Math.max(4,5,5,8,9,245));
+console.log(Math.random());
+
+
+
+
+
