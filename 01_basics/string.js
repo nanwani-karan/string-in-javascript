696921ce133a63f45277314bbed788f738b125aa
nanwani-karan <nanwanik21@gmail.com>
2025-05-23 06:29:53 +0000

    strings

---
+++ 01_basics/string.js
@@ -0,0 +1,17 @@
+
+const gamename = new String('karankc')
+
+//console.log(gamename[0]);
+//console.log(gamename.__proto__);
+
+//console.log(gamename.length);
+//console.log(gamename.toUpperCase());
+//0console.log(gamename.charAt(3));
+//console.log(gamename.indexOf('n'));
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'));
+console.log(url.includes('karan'));
+
+
+
